50f3fafad9753b6930053a3d2f0261b8d313623a
danbi98 <84821198+danbi98@users.noreply.github.com>
2021-06-15 16:49:05 +0900

    Create 5-4.py

---
+++ 5-4.py
@@ -0,0 +1,13 @@
+a  =  3
+b  =  2
+c  =  5
+d  =  c  -  a
+
+if  a  >  b :                        # a가 b보다 크면 Hello 0_0
+    print ( "Hello 0_0" )
+if  a  >  c :                         # a가 c보다 크면 What?
+    print ( "What?" )
+elif  a  ==  d :                      # a가 3과 같다면 bye ~!
+    print ( "안녕 ~!" )     
+else :                             # a가 b보다 작 으면 Hi
+    print ( "안녕하세요!" )
