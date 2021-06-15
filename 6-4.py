a0b7f7359e46927ddcfc2f6173b6a61b6335ea46
danbi98 <84821198+danbi98@users.noreply.github.com>
2021-06-15 16:51:34 +0900

    Create 6-4.py

---
+++ 6-4.py
@@ -0,0 +1,29 @@
+# a = 1
+# b = 2
+# c = a + b
+
+
+# x = 1
+# y = 2
+# z = x + y
+
+# def dsum (a, b) :
+# 결과 = a + b
+# 반환 결과
+
+# d = dsum (1, 2)
+# 인쇄 (d)
+
+# def dsum (a, b) :
+# 결과 = a + b
+# 인쇄 (결과)
+
+# d = dsum (1, 2)
+# 인쇄 (d)
+
+def  dsum ( a , b ) :
+    결과  =  a  +  b
+    반환  결과
+
+d  =  dsum ( 2 , 4 )
+인쇄 ( d )
