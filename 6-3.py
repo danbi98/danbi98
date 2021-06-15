0f8862b7b3b6f10ac133c854a765cef383845239
danbi98 <84821198+danbi98@users.noreply.github.com>
2021-06-15 16:51:06 +0900

    Create 6-3.py

---
+++ 6-3.py
@@ -0,0 +1,22 @@
+a  =  "정수"
+b  =  "태희"
+c  =  "환준"
+d  =  "세영"
+a1  =  20
+a2  =  19
+
+# def chat (name1, name2) : # 함수들이 인자라는 것을 포함합니다.
+# print ( "% s : 안녕? 넌 몇 살 이니?"% name1)
+# print ( "% s : 나? 나는 20 살이야!"% name2)
+
+# 채팅 (a, b)
+# 채팅 (c, d)
+
+
+# 5)
+def  chat ( name1 , name2 , age ) :         
+    print ( "% s : 안녕? 넌 몇 살 이니?"  %  name1 )
+    print ( "% s : 나? 나는 % d"  % ( name2 , age ) +  "살이야!" )
+
+채팅 ( "a" , "b" , a1 )
+채팅 ( "c" , "d" , a2 )
